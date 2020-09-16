574c91f743ade2488d749b0b645508a9901f5281
rrp86 <rrp86@njit.edu>
2020-09-16 14:14:46 -0400

    updating .gitignore

---
+++ HW2/src/Homework2.java
@@ -0,0 +1,18 @@
+public class Homework2
+{
+    public static void main(String[] args)
+    {
+        // 1. Create a variable that holds a name
+        String name1 = "Ritik";
+        // 2. Output the name to the console
+        System.out.println(name1);
+        // 3. Create a variable that holds an age (as a whole number)
+        int age1 = 20;
+        // 4. Output the age to the console
+        System.out.println(age1);
+        // 5. Create a variable that holds a decimal
+        double gpa1 = 3.3;
+        // 6. Output the variable
+        System.out.println(gpa1);
+    }
+}
