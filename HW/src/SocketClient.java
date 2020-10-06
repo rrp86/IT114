bebac88bccc22c3454003cf29be91e3545bdd5ee
rrp86 <rrp86@njit.edu>
2020-10-05 23:34:22 -0400

    Socket Client and Server Part 2

--- HW/src/SocketClient.java
+++ HW/src/SocketClient.java
@@ -6,77 +6,93 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+//part 2
 public class SocketClient {
 	Socket server;
+
 	public SocketClient() {
 
 	}
+
 	public void connect(String address, int port) {
 		try {
 			server = new Socket(address, port);
 			System.out.println("Client connected");
-		} 
-      catch (UnknownHostException e) {
+		} catch (UnknownHostException e) {
 			e.printStackTrace();
-		} 
-      catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 	public void start() throws IOException {
-		if(server == null) {
+		if (server == null) {
 			return;
 		}
 		System.out.println("Listening for input");
-		try(Scanner si = new Scanner(System.in);
+		try (Scanner si = new Scanner(System.in);
 				PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));){
+				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));) {
 			String line = "";
-			while(true) {
+			while (true) {
 				try {
 					System.out.println("Waiting for input");
 					line = si.nextLine();
-					if(!"quit".equalsIgnoreCase(line)) {
+					if (!"quit".equalsIgnoreCase(line)) {
 						out.println(line);
-					}
-					else {
+					} else {
 						break;
 					}
 					line = "";
-				}
-				catch(Exception e) {
+					String fromServer = in.readLine();
+
+					if (fromServer != null) {
+						System.out.println("Reply from server: " + fromServer);
+					} else {
+						System.out.println("Server disconnected");
+						break;
+					}
+				} catch (IOException e) {
 					System.out.println("Connection dropped");
 					break;
 				}
 			}
 			System.out.println("Exited loop");
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			close();
 		}
 	}
+
 	private void close() {
-		if(server != null) {
+		if (server != null) {
 			try {
 				server.close();
 				System.out.println("Closed socket");
-			} 
-         catch (IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 		SocketClient client = new SocketClient();
-		client.connect("127.0.0.1", 3001);//must match what server is listening to
+		int port = -1;
 		try {
-			//if start is private, it's valid here since this main is part of the class
+			// not safe but try-catch will get it
+			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			System.out.println("Invalid port");
+		}
+		if (port == -1) {
+			return;
+		}
+		client.connect("127.0.0.1", port);
+		try {
+			// if start is private, it's valid here since this main is part of the class
 			client.start();
-		} 
-      catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
